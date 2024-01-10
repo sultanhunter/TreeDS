a808fe2836b7985c997309f48a539398d5423fde
sultan <sultanibneusman0@gmail.com>
2024-01-11 01:26:54 +0530

    Initial Commit

---
+++ TreeDS/TreeDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeDSApp.swift
+//  TreeDS
+//
+//  Created by Sultan on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TreeDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
